987e3e19c99530617cf6edf1b36d90f3b371371b
Jason Antman <jason@jasonantman.com>
2015-01-09 15:30:29 -0500

    more skeleton, really just for travis testing

--- spec/unit/modulegetter_spec.rb
+++ spec/unit/modulegetter_spec.rb
@@ -15,5 +15,26 @@ EOF
   end
   let(:env)              { test_env.create_vagrant_env }
   let(:machine)          { env.machine(:test, :dummy) }
+  # Mock the communicator to prevent SSH commands for being executed.
+  let(:communicator)     { double('communicator') }
+  # Mock the guest operating system.
+  let(:guest)            { double('guest') }
+  let(:app)              { double('app') }
+  
+  before (:each) do
+    machine.stub(:guest => guest)
+    machine.stub(:communicator => communicator)
+  end
 
+  after(:each) { test_env.close }
+
+  context 'call' do
+    describe 'puppet_dir unset' do
+      it 'should raise an error' do
+        x = 1
+        x.should == 2
+      end
+    end
+  end
+  
 end
