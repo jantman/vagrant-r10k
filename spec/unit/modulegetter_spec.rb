1741c84f38ffa147383478a61a7a4af07cbcf472
Jason Antman <jason@jasonantman.com>
2015-01-10 07:40:08 -0500

    WIP - need to figure out DRY for specs

--- spec/unit/modulegetter_spec.rb
+++ spec/unit/modulegetter_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
+require_relative 'sharedcontext'
 
 require 'vagrant-r10k/modulegetter'
+require 'r10k/puppetfile'
 
 describe VagrantPlugins::R10k::Modulegetter do 
-  include_context 'vagrant-unit'
-  let(:test_env) do
-    test_env = isolated_environment
-    test_env.vagrantfile <<-EOF
+  subject { described_class.new(app, env) }
+
+  describe '#call' do
+    describe 'puppet_dir unset' do
+      include_context 'unit' do
+        let(:vagrantfile) { <<-EOF
 Vagrant.configure('2') do |config|
   config.vm.define :test
   # r10k plugin to deploy puppet modules
@@ -21,37 +25,128 @@ Vagrant.configure('2') do |config|
   end
 end
 EOF
-    test_env
-  end
-  let(:env)              { { env: iso_env, machine: machine, ui: ui, root_path: '/rootpath' } }
-  let(:conf)             { Vagrant::Config::V2::DummyConfig.new() }
-  let(:ui)               { Vagrant::UI::Basic.new() }
-  let(:iso_env)          { test_env.create_vagrant_env ui_class: Vagrant::UI::Basic }
-  let(:machine)          { iso_env.machine(:test, :dummy)  }
-  # Mock the communicator to prevent SSH commands for being executed.
-  let(:communicator)     { double('communicator') }
-  # Mock the guest operating system.
-  let(:guest)            { double('guest') }
-  let(:app)              { lambda { |env| } }
-  let(:plugin)           { register_plugin() }
- 
-  subject { described_class.new(app, env) }
-
-  before (:each) do
-    machine.stub(:guest => guest)
-    machine.stub(:communicator => communicator)
-  end
-
-  #after(:each) { test_env.close }
-  
-  describe '#call' do
-    describe 'puppet_dir unset' do
+        }
+      end
       it 'should raise an error' do
         expect(ui).to receive(:detail).with("vagrant-r10k: puppet_dir and/or puppetfile_path not set in config; not running").once
-        puts ui.methods(true)
-        subject.call(env)
+        expect(app).to receive(:call).with(env).once
+        File.stub(:join).and_call_original
+        File.stub(:join).with('/rootpath', 'puppet/Puppetfile').and_return('foobarbaz')
+        expect(File).to receive(:join).with('/rootpath', 'puppet/Puppetfile').exactly(0).times
+        # expect it to not really run
+        expect(ui).to receive(:info).with(/Beginning r10k deploy/).exactly(0).times
+        R10K::Puppetfile.stub(:new)
+        expect(R10K::Puppetfile).to receive(:new).exactly(0).times
+
+        retval = subject.call(env)
+        expect(retval).to be_nil
       end
     end
+    describe 'puppetfile_path unset' do
+      include_context 'unit' do
+        let(:vagrantfile) { <<-EOF
+Vagrant.configure('2') do |config|
+  config.vm.define :test
+  # r10k plugin to deploy puppet modules
+  config.r10k.puppet_dir = 'puppet'
+  # config.r10k.puppetfile_path = 'puppet/Puppetfile'
+
+  # Provision the machine with the appliction
+  config.vm.provision "puppet" do |puppet|
+    puppet.manifests_path = "puppet/manifests"
+    puppet.manifest_file  = "default.pp"
+    puppet.module_path = "puppet/modules"
   end
-  
+end
+EOF
+        }
+      end
+      it 'should raise an error' do
+        expect(ui).to receive(:detail).with("vagrant-r10k: puppet_dir and/or puppetfile_path not set in config; not running").once
+        expect(ui).to receive(:info).with(/Beginning r10k deploy/).exactly(0).times
+        expect(app).to receive(:call).with(env).once
+        File.stub(:join).and_call_original
+        File.stub(:join).with('/rootpath', 'puppet/Puppetfile').and_return('foobarbaz')
+        expect(File).to receive(:join).with('/rootpath', 'puppet/Puppetfile').exactly(0).times
+        # expect it to not really run
+        expect(ui).to receive(:info).with(/Beginning r10k deploy/).exactly(0).times
+        R10K::Puppetfile.stub(:new)
+        expect(R10K::Puppetfile).to receive(:new).exactly(0).times
+
+        retval = subject.call(env)
+        expect(retval).to be_nil
+      end
+    end
+    describe 'r10k module_path set differently from puppet' do
+      include_context 'unit' do
+        let(:vagrantfile) { <<-EOF
+Vagrant.configure('2') do |config|
+  config.vm.define :test
+  # r10k plugin to deploy puppet modules
+  config.r10k.puppet_dir = 'puppet'
+  config.r10k.puppetfile_path = 'puppet/Puppetfile'
+  config.r10k.module_path = "mymodulepath/foo"
+
+  # Provision the machine with the appliction
+  config.vm.provision "puppet" do |puppet|
+    puppet.manifests_path = "puppet/manifests"
+    puppet.manifest_file  = "default.pp"
+    puppet.module_path = "puppet/modules"
+  end
+end
+EOF
+        }
+      end
+      it 'should raise an error' do
+        expect(ui).to receive(:detail).with("vagrant-r10k: module_path \"mymodulepath/foo\" is not the same as in puppet provisioner; not running").once
+        expect(app).to receive(:call).with(env).once
+        File.stub(:file?).with('/rootpath/puppet/Puppetfile').and_return(true)
+        expect(File).to receive(:join).with('/rootpath', 'default.pp').exactly(0).times
+        # expect it to not really run
+        expect(ui).to receive(:info).with(/Beginning r10k deploy/).exactly(0).times
+        R10K::Puppetfile.stub(:new)
+        expect(R10K::Puppetfile).to receive(:new).exactly(0).times
+        
+        retval = subject.call(env)
+        expect(retval).to be_nil
+      end
+    end
+    describe 'r10k module_path not set' do
+      include_context 'unit' do
+        let(:vagrantfile) { <<-EOF
+Vagrant.configure('2') do |config|
+  config.vm.define :test
+  # r10k plugin to deploy puppet modules
+  config.r10k.puppet_dir = 'puppet'
+  config.r10k.puppetfile_path = 'puppet/Puppetfile'
+
+  # Provision the machine with the appliction
+  config.vm.provision "puppet" do |puppet|
+    puppet.manifests_path = "puppet/manifests"
+    puppet.manifest_file  = "default.pp"
+    puppet.module_path = "puppet/modules"
+  end
+end
+EOF
+        }
+      end
+      it 'should use the module_path from provisioner' do
+        expect(ui).to receive(:info).with("vagrant-r10k: Building the r10k module path with puppet provisioner module_path \"puppet/modules\". (if module_path is an array, first element is used)").once
+        File.stub(:file?).with('/rootpath/puppet/Puppetfile').and_return(true)
+        expect(ui).to receive(:info).with(/Beginning r10k deploy/).once
+        R10K::Puppetfile.stub(:new)
+        expect(R10K::Puppetfile).to receive(:new).with('/rootpath/puppet', '/rootpath/puppet/modules', '/rootpath/puppet/Puppetfile').once
+        R10K::Task::Puppetfile::Sync.stub(:new)
+        expect(R10K::Task::Puppetfile::Sync).to receive(:new).once
+        R10K::TaskRunner.stub(:append_task)
+        R10K::TaskRunner.stub(:run)
+        expect(R10K::TaskRunner).to receive(:append_task).once
+        expect(R10K::TaskRunner).to receive(:run).once
+        retval = subject.call(env)
+        expect(retval).to be_nil
+      end
+    end
+
+    
+  end
 end
---
+++ spec/unit/sharedcontext.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+shared_context "unit" do
+  include_context 'vagrant-unit'
+
+  let(:test_env) do
+    test_env = isolated_environment
+    test_env.vagrantfile vagrantfile
+    test_env
+  end
+  let(:env)              { { env: iso_env, machine: machine, ui: ui, root_path: '/rootpath' } }
+  let(:conf)             { Vagrant::Config::V2::DummyConfig.new() }
+  let(:ui)               { Vagrant::UI::Basic.new() }
+  let(:iso_env)          { test_env.create_vagrant_env ui_class: Vagrant::UI::Basic }
+  let(:machine)          { iso_env.machine(:test, :dummy)  }
+  # Mock the communicator to prevent SSH commands for being executed.
+  let(:communicator)     { double('communicator') }
+  # Mock the guest operating system.
+  let(:guest)            { double('guest') }
+  let(:app)              { lambda { |env| } }
+  let(:plugin)           { register_plugin() }
+
+  before (:each) do
+    machine.stub(:guest => guest)
+    machine.stub(:communicator => communicator)
+  end
+
+end
