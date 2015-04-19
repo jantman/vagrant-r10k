6d5501e136e7834504c02b42dfae17bb0f60a9a6
Jason Antman <jason@jasonantman.com>
2015-04-18 21:38:14 -0400

    move some specs to try and track down order-dependent spec test failures

---
+++ spec/unit/log4r_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'vagrant-r10k/modulegetter'
+
+describe Log4r::Logger do
+  subject { described_class.new('testlogger') }
+  describe '#debug1' do
+    it 'should pass through to debug' do
+      expect(subject).to receive(:debug).with('a message').once
+      subject.debug1('a message')
+    end
+  end
+  describe '#debug2' do
+    it 'should pass through to debug' do
+      expect(subject).to receive(:debug).with('different message').once
+      subject.debug2('different message')
+    end
+  end 
+end
