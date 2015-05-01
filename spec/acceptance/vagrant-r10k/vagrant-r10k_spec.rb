c270b5a43f9f2a11559e70cbe00bfecf37f72913
Jason Antman <jason@jasonantman.com>
2015-05-01 06:17:25 -0400

    cleanup after tests is now working fine; ready to actually work on the tests

--- spec/acceptance/vagrant-r10k/vagrant-r10k_spec.rb
+++ spec/acceptance/vagrant-r10k/vagrant-r10k_spec.rb
@@ -11,26 +11,26 @@ shared_examples 'provider/vagrant-r10k' do |provider, options|
   let(:name)   { 'single.testbox.spec' }
 
   before do
-    assert_execute('vagrant', 'box', 'add', 'box', options[:box])
+    assert_execute('vagrant', 'box', 'add', "vagrantr10kspec", options[:box])
     ENV['VAGRANT_DEFAULT_PROVIDER'] = provider
   end
 
   describe 'configured correctly' do
     before do
       environment.skeleton('correct')
+      puts "Isolated Environment: homedir=#{environment.homedir} workdir=#{environment.workdir}"
+    end
+
+    after do
+      assert_execute("vagrant", "destroy", "--force")
     end
 
     it 'deploys once' do
       #result = assert_execute('vagrant', 'up', "--provider=#{provider}", "--debug")
       result = assert_execute('vagrant', 'up', "--provider=#{provider}")
       expect(result).to exit_with(0)
-      expect(result.stdout).to include('foobarbaz')
-      expect(result.stderr).to include('foobarbaz')
+      expect(result.stdout).to include('r10k')
+      expect(result.stderr).to match(//)
     end
-    it 'cleanup' do
-      puts "Destroying vagrant machine..."
-      result = assert_execute("vagrant destroy -f")
-    end
-
   end
 end
